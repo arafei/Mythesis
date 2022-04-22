e2ad05a0a2e693641e84f1e5af25d7329233d5f1
Ali Rafei <33495530+arafei@users.noreply.github.com>
2022-04-21 23:29:37 -0700

    Update func_AIPW.R

--- Chapter3/Application/func_AIPW.R
+++ Chapter3/Application/func_AIPW.R
@@ -32,7 +32,7 @@
 # Inputs of AIPW_KH:
 # y is a vector of outcome; NA for the probability sample (n x 1)
 # x is a matrix of covariates without intercept  (n x p)
-# deltaR is a vector of the binary indicator of belonging to the nonprobability sample;
+# deltaA is a vector of the binary indicator of belonging to the nonprobability sample;
 # i.e., 1 if the unit belongs to the nonprobability sample, and 0 otherwise  (n x 1)
 # sw is a vector of weights: 1 for the unit in the nonprobability sample and the 
 # design weight for the unit in the probability sample  (n x 1) family specifies 
@@ -44,13 +44,11 @@
 # spec: Model specification status (TRUE, FALSE) used for the simulation
 # Method = c('PAPP', 'PMLE', 'IPSW', 'PM', 'AIPW')
 #
-# @return
-# \itemize{
-# \item \code{Method}: The name of the method used
-# \item \code{Mu}:  adjusted mean
-# \item \code{SE}: adjusted SE
-# \item \code{RW}: the associated estimated pseudo weights for units of S_A
-# }
+# return:
+# Method: The name of the method used
+# Mu: adjusted mean
+# SE: adjusted SE
+# RW: the associated estimated pseudo weights for units of S_A
 
 # install.packages("rootSolve")  # @import MASS rootSolve ncvreg stats
 library(rootSolve)  #needed for multiroot() to solve joint estimating equations
@@ -59,9 +57,10 @@ library(ggplot2)
 library(gridExtra)
 library(survey)
 library(gtools)
+library(betareg)
 library(MASS)
 
-AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IPSW"), family=c('gaussian', 'binomial')){
+AIPW_KH <- function(y, x, deltaA, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IPSW"), family=c('gaussian', 'binomial')){
 
 # Model specification in the simulation  
 	PS_f <- PM_f <- colnames(x)
@@ -72,33 +71,33 @@ AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IP
 	x.ARm <- as.matrix(cbind(1, x[, PM_f])) # Pooled design martix for PM
 	y.AR <- y # Pooled ouctome: NA for units of S_R
 	y.AR[which(is.na(y.AR))] <- 0 # Set NAs to 0
-	nA <- sum(1-deltaR) # Sample size in S_A
-	nR <- sum(deltaR) # Sample size in S_R
+	nA <- sum(deltaA) # Sample size in S_A
+	nR <- sum(1-deltaA) # Sample size in S_R
 	nAR <- nA + nR # Total sample size
-	loc.A <- which(deltaR==0) # Indicators of being in S_A
-	loc.R <- which(deltaR==1) # Indicators of being in S_R
+	loc.A <- which(deltaA==1) # Indicators of being in S_A
+	loc.R <- which(deltaA==0) # Indicators of being in S_R
 	x.Rp <- x.ARp[loc.R,] # X of PS model in S_R
 	x.Rm <- x.ARm[loc.R,] # X of PM model in S_R
 	x.Ap <- x.ARp[loc.A,] # X of PS model in S_A
 	x.Am <- x.ARm[loc.A,] # X of PM model in S_A
-	y.R <- y.AR[loc.R] # Outcome in S_R
-	N <- sum(sw[loc.A]) # Estimated population size
+	y.A <- y.AR[loc.A] # Outcome in S_A
+	N <- sum(sw[loc.R]) # Estimated population size
 	sw.A <- sw[loc.A] # Sampling weights of S_R in S_A
-	sw.B <- sw[loc.R] # Sampling weights of S_R in S_R
+	sw.R <- sw[loc.R] # Sampling weights of S_R in S_R
 
 	nms <- out_m <- out_se <- pwght <- c() #empty oututs
 	
 	# Estimate propensity weighted estimate and associated asmptotic SE based on PAPP
 	if("PAPP"%in%method){
-		fit0 <- glm(z~., data=data.frame(X=x.ARp[,-1], z=deltaR), family=binomial(link="logit"))  # Fit P(Z_i=1|X)
+		fit0 <- glm(z~., data=data.frame(X=x.ARp[,-1], z=deltaA), family=binomial(link="logit"))  # Fit P(Z_i=1|X)
 		ei <- exp(predict(fit0, type="link"))
-		weight <- sw.B/ei[loc.R]  # Estimate PAPP weights
-		est.pdr <- weighted.mean(y.R, w=weight, na.rm=T) # Comput weighted estimates
+		weight <- sw.A/ei[loc.A]  # Estimate PAPP weights
+		est.pdr <- weighted.mean(y.A, w=weight, na.rm=T) # Comput weighted estimates
 		# Compute asymptotic SE using TSL method
-		wd <- (y.R-est.pdr)*weight
-		b <- (t(wd)%*%x.ARp[loc.R, ]/N)%*%ginv(t(x.ARp)%*%(x.ARp*ei/(N*((1+ei)^2))))
+		wd <- (y.A-est.pdr)*weight
+		b <- (t(wd)%*%x.ARp[loc.A, ]/N)%*%ginv(t(x.ARp)%*%(x.ARp*ei/(N*((1+ei)^2))))
 		A <- (1/N^2)*sum((1-1/weight)*(wd^2))
-		B <- (t(wd/(1+ei[loc.R]))%*%x.ARp[loc.R, ]/(N^2))%*%t(b)
+		B <- (t(wd/(1+ei[loc.A]))%*%x.ARp[loc.A, ]/(N^2))%*%t(b)
 		C <- b%*%(t(x.ARp)%*%(x.ARp*ei/((N^2)*((1+ei)^2))))%*%t(b)
 		se.pdr <- sqrt(A - 2*B + C)
 		out_m <- c(out_m, est.pdr)
@@ -108,16 +107,15 @@ AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IP
 	}
 	if("PMLE"%in%method){
 		W <- sw
-		W[loc.R] <- 1
-		PZi <- chen_glm(X=x.ARp[,-1], Y=deltaR, di=W)$predict  #Fit PS model based on Chen et al's PMLE method
-		weight <- 1/PZi[loc.R] # Estimate PMLE weights
+		W[loc.A] <- 1
+		PZi <- chen_glm(X=x.ARp[,-1], Y=deltaA, di=W)$predict  #Fit PS model based on Chen et al's PMLE method
+		weight <- 1/PZi[loc.A] # Estimate PMLE weights
 		# Compute asymptotic SE using TSL method 
-		est.pdr <- weighted.mean(y.R, w=weight, na.rm=T)
-		wd <- (y.R-est.pdr)*weight
-		b <- (t((weight-1)*(y.R-est.pdr))%*%x.ARp[loc.R, ])%*%ginv(t(x.ARp[loc.A, ])%*%(x.ARp[loc.A, ]*sw.A*PZi[loc.A]*(1-PZi[loc.A])))
-		D <- nA*cov(PZi[loc.A]*sw.A*x.ARp[loc.A, ])/(N^2)
-		#D <- t((sw.A^2 - sw.A) * x.ARp[loc.A, ]) %*% x.ARp[loc.A, ]/(N^2)
-		se.pdr <- sqrt((1/N^2)*sum((1-1/weight)*((wd-x.ARp[loc.R, ]%*%t(b))^2)) + b%*%D%*%t(b))
+		est.pdr <- weighted.mean(y.A, w=weight, na.rm=T)
+		wd <- (y.A-est.pdr)*weight
+		b <- (t((weight-1)*(y.A-est.pdr))%*%x.ARp[loc.A, ])%*%ginv(t(x.ARp[loc.R, ])%*%(x.ARp[loc.R, ]*sw.R*PZi[loc.R]*(1-PZi[loc.R])))
+		D <- nR*cov(PZi[loc.R]*sw.R*x.ARp[loc.R, ])/(N^2)
+		se.pdr <- sqrt((1/N^2)*sum((1-1/weight)*((wd-x.ARp[loc.A, ]%*%t(b))^2)) + b%*%D%*%t(b))
 		out_m <- c(out_m, est.pdr)
 		out_se <- c(out_se, se.pdr)
 		pwght <- cbind(pwght, weight)
@@ -125,15 +123,14 @@ AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IP
 	}
 	if("IPSW"%in%method){
 		W <- sw
-		W[loc.R] <- 1
-		PZi <- wglm(X=x.ARp[,-1], Y=deltaR, di=W)$predict
-		weight <- (1-PZi[loc.R])/PZi[loc.R]
-		est.pdr <- weighted.mean(y.R, w=weight, na.rm=T)
-		wd <- (y.R-est.pdr)*weight
-		b <- (t((est.pdr-y.R)/weight)%*%x.ARp[loc.R, ])%*%ginv(t(x.ARp[loc.R, ])%*%(x.ARp[loc.R, ]*PZi[loc.R]))
-		D <- nA*cov(PZi[loc.A]*sw.A*x.ARp[loc.A, ])/(N^2)
-		#D <- t((sw.A^2 - sw.A) * x.ARp[loc.A, ]) %*% x.ARp[loc.A, ]/(N^2)
-		se.pdr <- sqrt((1/N^2)*sum((1-1/weight)*((wd-x.ARp[loc.R, ]%*%t(b))^2)) + b%*%D%*%t(b))
+		W[loc.A] <- 1
+		PZi <- wglm(X=x.ARp[,-1], Y=deltaA, di=W)$predict
+		weight <- (1-PZi[loc.A])/PZi[loc.A]
+		est.pdr <- weighted.mean(y.A, w=weight, na.rm=T)
+		wd <- (y.A-est.pdr)*weight
+		b <- (t((est.pdr-y.A)/weight)%*%x.ARp[loc.A, ])%*%ginv(t(x.ARp[loc.R, ])%*%(x.ARp[loc.R, ]*PZi[loc.R]))
+		D <- nR*cov(PZi[loc.R]*sw.R*x.ARp[loc.R, ])/(N^2)
+		se.pdr <- sqrt((1/N^2)*sum((1-1/weight)*((wd-x.ARp[loc.A, ]%*%t(b))^2)) + b%*%D%*%t(b))
 		out_m <- c(out_m, est.pdr)
 		out_se <- c(out_se, se.pdr)
 		pwght <- cbind(pwght, weight)
@@ -141,20 +138,20 @@ AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IP
 	}
 	if("PM"%in%method){
 		if(family=="gaussian"){
-			fit1 <- lm(y~., data=data.frame(x.Rm[,-1], y=y.R))
-			pred <- predict(fit1, newdata=data.frame(x.Am[,-1]),type="response",se.fit = TRUE)
+			fit1 <- lm(y~., data=data.frame(x.Am[,-1], y=y.A))
+			pred <- predict(fit1, newdata=data.frame(x.Rm[,-1]),type="response",se.fit = TRUE)
 			m.A <- pred$fit
-			est.pdr <- weighted.mean(m.A, w=sw.A, na.rm=T)
-			c <- ginv(t(x.ARm[loc.R, ])%*%x.ARm[loc.R, ]/nR)%*%t(t(sw.A)%*%x.ARm[loc.A, ]/N)
-			ve.pdr <- weighted.var(y0=m.A, w0=sw.A) + sum((fit1$residuals^2)*(x.ARm[loc.R, ]%*%c)^2)/(nR^2)
+			est.pdr <- weighted.mean(m.A, w=sw.R, na.rm=T)
+			c <- ginv(t(x.ARm[loc.A, ])%*%x.ARm[loc.A, ]/nA)%*%t(t(sw.R)%*%x.ARm[loc.R, ]/N)
+			ve.pdr <- weighted.var(y0=m.A, w0=sw.R) + sum((fit1$residuals^2)*(x.ARm[loc.A, ]%*%c)^2)/(nA^2)
 			se.pdr<-sqrt(ve.pdr)
 		}else if(family=="binomial"){
-			fit1 <- glm(y~., data=data.frame(x.Rm[,-1], y=y.R), family=binomial(link="logit"))
-			m.A <- predict(fit1, newdata=data.frame(x.Am[,-1]), type="response")
+			fit1 <- glm(y~., data=data.frame(x.Am[,-1], y=y.A), family=binomial(link="logit"))
+			m.A <- predict(fit1, newdata=data.frame(x.Rm[,-1]), type="response")
 			m.B <- predict(fit1, type="response")
-			est.pdr <- weighted.mean(m.A, w=sw[loc.A], na.rm=T)
-			c <- ginv(t(x.ARm[loc.R, ])%*%x.ARm[loc.R, ]/nR)%*%t(t(sw.A)%*%x.ARm[loc.A, ]/N)
-			ve.pdr <- weighted.var(y0=m.A, w0=sw.A) + sum((fit1$residuals^2)*((x.ARm[loc.R, ]*m.B*(1-m.B))%*%c)^2)/(nR^2)
+			est.pdr <- weighted.mean(m.A, w=sw[loc.R], na.rm=T)
+			c <- ginv(t(x.ARm[loc.A, ])%*%x.ARm[loc.A, ]/nA)%*%t(t(sw.R)%*%x.ARm[loc.R, ]/N)
+			ve.pdr <- weighted.var(y0=m.A, w0=sw.R) + sum((fit1$residuals^2)*((x.ARm[loc.A, ]*m.B*(1-m.B))%*%c)^2)/(nA^2)
 			se.pdr<-sqrt(ve.pdr)			
 		}
 		out_m <- c(out_m, est.pdr)
@@ -168,9 +165,9 @@ AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IP
 			##################################################
 
 			par0 <- rep(0, (pp + pm + 2))
-			par0[1:(pp+1)] <- glm(z~., data=data.frame(X=x.ARp[,-1], z=deltaR))$coefficients
-			par0[(pp+2):(pp + pm + 2)] <- lm(y~., data=data.frame(X=x.Rm[,-1], y=y.R))$coefficients
-			jeepar<-rootSolve::multiroot(Uee_papp,par0,deltaR=deltaR,x.ARp=x.ARp,x.ARm=x.ARm,y.AR=y.AR,sw=sw)$root
+			par0[1:(pp+1)] <- glm(z~., data=data.frame(X=x.ARp[,-1], z=deltaA))$coefficients
+			par0[(pp+2):(pp + pm + 2)] <- lm(y~., data=data.frame(X=x.Am[,-1], y=y.A))$coefficients
+			jeepar<-rootSolve::multiroot(Uee_papp,par0,deltaA=deltaA,x.ARp=x.ARp,x.ARm=x.ARm,y.AR=y.AR,sw=sw)$root
 			jeepar1<-jeepar[ 1:(pp+1)]
 			jeepar2<-jeepar[(pp+2):(pp + pm + 2)]
 			lpi.est <- exp(as.vector(x.ARp %*% as.matrix(jeepar1)))
@@ -181,11 +178,10 @@ AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IP
 			##################################################
 			## doubly robust estimation for FPM
 			##################################################
-			est.pdr <- sum((y.AR-m.est)*deltaR/piB.est)/N + sum(m.est*(1-deltaR)*sw)/N
-			m.A <- m.est[loc.A]
-			sigmasqhat <- sum((y.AR[loc.R]-m.est[loc.R])^2)/(nR-pm-1)
-			W2 <-   #nA*var(m.est[loc.A]*sw.A)/(N^2)  #sum((sw.A^2 - sw.A) * (m.A)^2)/(N^2)
-			ve.pdr <- weighted.var(y0=m.A, w0=sw.A) + (sum(deltaR*(1-2*piB.est )/piB.est ^2) + (N )) * sigmasqhat/(N^2)
+			est.pdr <- sum((y.AR-m.est)*deltaA/piB.est)/N + sum(m.est*(1-deltaA)*sw)/N
+			m.A <- m.est[loc.R]
+			sigmasqhat <- sum((y.AR[loc.A]-m.est[loc.A])^2)/(nA-pm-1)
+			ve.pdr <- weighted.var(y0=m.A, w0=sw.R) + (sum(deltaA*(1-2*piB.est )/piB.est ^2) + (N )) * sigmasqhat/(N^2)
 			se.pdr<-sqrt(ve.pdr)
 
 		}else if(family=="binomial"){
@@ -193,9 +189,9 @@ AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IP
 			## joint estimating equations
 			##################################################
 			par0 <- rep(0, (pp + pm + 2))
-			par0[1:(pp+1)] <- glm(z~., data=data.frame(X=x.ARp[,-1], z=deltaR), family=binomial(link="logit"))$coefficients
-			par0[(pp+2):(pp+pm+2)] <- glm(y~., data=data.frame(X=x.Rm[,-1], y=y.R), family=binomial(link="logit"))$coefficients
-			jeepar<-rootSolve::multiroot(Uee_binary_papp,par0,deltaR=deltaR,x.ARp=x.ARp,x.ARm=x.ARm,y.AR=y.AR,sw=sw)$root
+			par0[1:(pp+1)] <- glm(z~., data=data.frame(X=x.ARp[,-1], z=deltaA), family=binomial(link="logit"))$coefficients
+			par0[(pp+2):(pp+pm+2)] <- glm(y~., data=data.frame(X=x.Am[,-1], y=y.A), family=binomial(link="logit"))$coefficients
+			jeepar<-rootSolve::multiroot(Uee_binary_papp,par0,deltaA=deltaA,x.ARp=x.ARp,x.ARm=x.ARm,y.AR=y.AR,sw=sw)$root
 			jeepar1<-jeepar[ 1:(pp + 1)]
 			jeepar2<-jeepar[(pp + 2):(pp + pm + 2)]
 			lpi.est <- exp(as.vector(x.ARp %*% as.matrix(jeepar1)))
@@ -207,15 +203,15 @@ AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IP
 			##################################################
 			## doubly robust estimation for FPM
 			##################################################
-			est.pdr <- sum((y.AR-m.est)*deltaR/piB.est)/N+(sum(m.est*(1-deltaR)*sw))/N
-			m.A <- m.est[loc.A]
+			est.pdr <- sum((y.AR-m.est)*deltaA/piB.est)/N+(sum(m.est*(1-deltaA)*sw))/N
+			m.A <- m.est[loc.R]
 			sigmasqhat <- m.est*(1-m.est)
-			ve.pdr <- weighted.var(y0=m.A, w0=sw.A) + (sum(sigmasqhat*deltaR*(1-2*piB.est )/piB.est ^2) + sum(sigmasqhat[loc.A]/sw.A))/(N^2)
+			ve.pdr <- weighted.var(y0=m.A, w0=sw.R) + (sum(sigmasqhat*deltaA*(1-2*piB.est )/piB.est ^2) + sum(sigmasqhat[loc.R]/sw.R))/(N^2)
 			se.pdr<-sqrt(ve.pdr)
 		}
 		out_m <- c(out_m, est.pdr)
 		out_se <- c(out_se, se.pdr)
-		pwght <- cbind(pwght, 1/piB.est[loc.R])
+		pwght <- cbind(pwght, 1/piB.est[loc.A])
 		nms <- c(nms, "AIPW_PAPP")
 	}
 	if("PMLE"%in%method & "AIPW"%in%method){
@@ -226,10 +222,10 @@ AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IP
 			##################################################
 			par0 <- rep(0, (pp + pm + 2))
 			W <- sw
-			W[loc.R] <- 1
-			par0[1:(pp+1)] <- chen_glm(X=x.ARp[,-1], Y=deltaR, di=W)$coef
-			par0[(pp+2):(pp + pm + 2)] <- lm(y~., data=data.frame(X=x.Rm[,-1], y=y.R))$coefficients
-			jeepar<-rootSolve::multiroot(Uee_pmle,par0,deltaR=deltaR,x.ARp=x.ARp,x.ARm=x.ARm,y.AR=y.AR,sw=sw)$root
+			W[loc.A] <- 1
+			par0[1:(pp+1)] <- chen_glm(X=x.ARp[,-1], Y=deltaA, di=W)$coef
+			par0[(pp+2):(pp + pm + 2)] <- lm(y~., data=data.frame(X=x.Am[,-1], y=y.A))$coefficients
+			jeepar<-rootSolve::multiroot(Uee_pmle,par0,deltaA=deltaA,x.ARp=x.ARp,x.ARm=x.ARm,y.AR=y.AR,sw=sw)$root
 			jeepar1<-jeepar[ 1:(pp+1)]
 			jeepar2<-jeepar[(pp+2):(pp + pm + 2)]
 			lpi.est <- exp(as.vector(x.ARp %*% as.matrix(jeepar1)))
@@ -239,10 +235,10 @@ AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IP
 			##################################################
 			## doubly robust estimation for FPM
 			##################################################
-			est.pdr <- sum((y.AR-m.est)*deltaR/piB.est)/N + sum(m.est*(1-deltaR)*sw)/N
-			m.A <- m.est[loc.A]
-			sigmasqhat <- sum((y.AR[loc.R]-m.est[loc.R])^2)/(nR-pm-1)
-			ve.pdr <- weighted.var(y0=m.A, w0=sw.A) + (sum(deltaR*(1-2*piB.est )/piB.est ^2) + (N )) * sigmasqhat/(N^2)
+			est.pdr <- sum((y.AR-m.est)*deltaA/piB.est)/N + sum(m.est*(1-deltaA)*sw)/N
+			m.A <- m.est[loc.R]
+			sigmasqhat <- sum((y.AR[loc.A]-m.est[loc.A])^2)/(nA-pm-1)
+			ve.pdr <- weighted.var(y0=m.A, w0=sw.R) + (sum(deltaA*(1-2*piB.est )/piB.est ^2) + (N )) * sigmasqhat/(N^2)
 			se.pdr<-sqrt(ve.pdr)
 
 		}else if(family=="binomial"){
@@ -252,10 +248,10 @@ AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IP
 			##################################################
 			par0 <- rep(0, (pp + pm + 2))
 			W <- sw
-			W[loc.R] <- 1
-			par0[1:(pp+1)] <- chen_glm(X=x.ARp[,-1], Y=deltaR, di=W)$coef
-			par0[(pp+2):(pp+pm+2)] <- glm(y~., data=data.frame(X=x.Rm[,-1], y=y.R), family=binomial(link="logit"))$coefficients
-			jeepar<-rootSolve::multiroot(Uee_binary_pmle,par0,deltaR=deltaR,x.ARp=x.ARp,x.ARm=x.ARm,y.AR=y.AR,sw=sw)$root
+			W[loc.A] <- 1
+			par0[1:(pp+1)] <- chen_glm(X=x.ARp[,-1], Y=deltaA, di=W)$coef
+			par0[(pp+2):(pp+pm+2)] <- glm(y~., data=data.frame(X=x.Am[,-1], y=y.A), family=binomial(link="logit"))$coefficients
+			jeepar<-rootSolve::multiroot(Uee_binary_pmle,par0,deltaA=deltaA,x.ARp=x.ARp,x.ARm=x.ARm,y.AR=y.AR,sw=sw)$root
 			jeepar1<-jeepar[ 1:(pp + 1)]
 			jeepar2<-jeepar[(pp + 2):(pp + pm + 2)]
 			lpi.est <- exp(as.vector(x.ARp %*% as.matrix(jeepar1)))
@@ -266,14 +262,14 @@ AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IP
 			##################################################
 			## doubly robust estimation for FPM
 			##################################################
-			est.pdr <- sum((y.AR-m.est)*deltaR/piB.est)/N+(sum(m.est*(1-deltaR)*sw))/N
-			m.A <- m.est[loc.A]
+			est.pdr <- sum((y.AR-m.est)*deltaA/piB.est)/N+(sum(m.est*(1-deltaA)*sw))/N
+			m.A <- m.est[loc.R]
 			sigmasqhat <- m.est*(1-m.est)
-			ve.pdr <- weighted.var(y0=m.A, w0=sw.A) + (sum(sigmasqhat*deltaR*(1-2*piB.est )/piB.est ^2) + sum(sigmasqhat[loc.A]/sw.A))/(N^2)
+			ve.pdr <- weighted.var(y0=m.A, w0=sw.R) + (sum(sigmasqhat*deltaA*(1-2*piB.est )/piB.est ^2) + sum(sigmasqhat[loc.R]/sw.R))/(N^2)
 		}
 		out_m <- c(out_m, est.pdr)
 		out_se <- c(out_se, se.pdr)
-		pwght <- cbind(pwght, 1/piB.est[loc.R])
+		pwght <- cbind(pwght, 1/piB.est[loc.A])
 		nms <- c(nms, "AIPW_PMLE")
 	}
 	colnames(pwght) <- nms[nms!="PM"]
@@ -287,85 +283,85 @@ AIPW_KH <- function(y, x, deltaR, sw, method=c("PAPP", "PMLE", "IPSW", "PM", "IP
 ####################################################################################
 
 # joint score equation for alpha and beta of continuous outcome under PAPP
-Uee_papp <- function(par, deltaR, x.ARp, x.ARm, y.AR, sw){
+Uee_papp <- function(par, deltaA, x.ARp, x.ARm, y.AR, sw){
 	pp<-ncol(x.ARp)-1
 	pm<-ncol(x.ARm)-1
-	nAR <- length(deltaR)
+	nAR <- length(deltaA)
 	alpha <- par[1:(pp+1)]
 	beta <- par[(pp+2):(pp + pm + 2)]
-	N<- sum(sw[deltaR==0])
+	N<- sum(sw[deltaA==0])
 	lpiZ <- exp(x.ARp%*%alpha)
 	piZ <- lpiZ/(1+lpiZ)
-	deltaA <- 1-deltaR
+	deltaA <- 1-deltaA
 	y.AR[which(is.na(y.AR))] <- 0
 	resAB <- (y.AR-x.ARm%*%beta)
 	piZ <- as.vector(piZ)
 	sw <- as.vector(sw)
 	resAB <- as.vector(resAB)
-	c(apply(x.ARm*(deltaR/piZ-1)*sw,2,sum), apply(x.ARp*deltaR*(1/piZ-1)*resAB*sw,2,sum))/N
+	c(apply(x.ARm*(deltaA/piZ-1)*sw,2,sum), apply(x.ARp*deltaA*(1/piZ-1)*resAB*sw,2,sum))/N
 }
 
 # joint score equation for alpha and beta of continuous outcome under PMLE
-Uee_pmle <- function(par, deltaR, x.ARp, x.ARm, y.AR, sw){
+Uee_pmle <- function(par, deltaA, x.ARp, x.ARm, y.AR, sw){
 	pp<-ncol(x.ARp)-1
 	pm<-ncol(x.ARm)-1
-	nAR <- length(deltaR)
+	nAR <- length(deltaA)
 	alpha <- par[1:(pp+1)]
 	beta <- par[(pp+2):(pp + pm + 2)]
-	N<- sum(sw[deltaR==0])
+	N<- sum(sw[deltaA==0])
 	lpiB <- exp(x.ARp%*%alpha)
 	piB <- lpiB/(1+lpiB)
-	deltaA <- 1-deltaR
+	deltaA <- 1-deltaA
 	y.AR[which(is.na(y.AR))] <- 0
 	resAB <- (y.AR-x.ARm%*%beta)
 	piB <- as.vector(piB)
 	sw <- as.vector(sw)
 	resAB <- as.vector(resAB)
-	c(apply(x.ARm*deltaR/piB-x.ARm*deltaA*sw,2,sum), apply(x.ARp*deltaR*(1/piB-1)*resAB,2,sum))/nAR
+	c(apply(x.ARm*deltaA/piB-x.ARm*deltaA*sw,2,sum), apply(x.ARp*deltaA*(1/piB-1)*resAB,2,sum))/nAR
 }
 
 # joint score equation for alpha and beta of binary outcome under PAPP
-Uee_binary_papp <- function(par,deltaR,x.ARp,x.ARm,y.AR,sw){
+Uee_binary_papp <- function(par,deltaA,x.ARp,x.ARm,y.AR,sw){
 	pp<-ncol(x.ARp)-1
 	pm<-ncol(x.ARm)-1
 	if(is.null(pp))pp=1
 	if(is.null(pm))pm=1
-	nAR<-length(deltaR)
+	nAR<-length(deltaA)
 	alpha<-par[1:(pp+1)]
 	beta <-par[(pp+2):(pp+pm+2)]
-	N<- sum(sw[deltaR==0])
+	N<- sum(sw[deltaA==0])
 	lpiZ<-x.ARp%*%alpha
 	piZ <-exp(lpiZ)/(1+exp(lpiZ))
-	deltaA<-1-deltaR
+	deltaA<-1-deltaA
 	mAB<-expoit(x.ARm%*%beta)
 	resAB<-(y.AR- mAB)
 	piZ<-as.vector(piZ)
 	sw<-as.vector(sw)
 	resAB<-as.vector(resAB)
 	mAB<-as.vector(mAB)
-	c(apply(x.ARm*mAB*(1-mAB)*(deltaR/piZ-1)*sw,2,sum), apply(x.ARp*deltaR*(1/piZ-1)*resAB*sw, 2, sum))/N
+	c(apply(x.ARm*mAB*(1-mAB)*(deltaA/piZ-1)*sw,2,sum), apply(x.ARp*deltaA*(1/piZ-1)*resAB*sw, 2, sum))/N
 }
 
 # joint score equation for alpha and beta of binary outcome under PMLE
-Uee_binary_pmle <- function(par,deltaR,x.ARp,x.ARm,y.AR,sw){
+Uee_binary_pmle <- function(par,deltaA,x.ARp,x.ARm,y.AR,sw){
 	pp<-ncol(x.ARp)-1
 	pm<-ncol(x.ARm)-1
 	if(is.null(pp))pp=1
 	if(is.null(pm))pm=1
-	nAR<-length(deltaR)
+	nAR<-length(deltaA)
 	alpha<-par[1:(pp+1)]
 	beta <-par[(pp+2):(pp+pm+2)]
-	N<- sum(sw[deltaR==0])
+	N<- sum(sw[deltaA==0])
 	lpiB<-x.ARp%*%alpha
 	piB <-exp(lpiB)/(1+exp(lpiB))
-	deltaA<-1-deltaR
+	deltaA<-1-deltaA
 	mAB<-expoit(x.ARm%*%beta)
 	resAB<-(y.AR- mAB)
 	piB<-as.vector(piB)
 	sw<-as.vector(sw)
 	resAB<-as.vector(resAB)
 	mAB<-as.vector(mAB)
-	c(apply(x.ARm*deltaR/piB*mAB*(1-mAB) -x.ARm*deltaA*sw*mAB*(1-mAB),2,sum), apply(x.ARp*deltaR*(1/piB-1)*resAB,2,sum))/nAR
+	c(apply(x.ARm*deltaA/piB*mAB*(1-mAB) -x.ARm*deltaA*sw*mAB*(1-mAB),2,sum), apply(x.ARp*deltaA*(1/piB-1)*resAB,2,sum))/nAR
 }
 
 # Logistic function
